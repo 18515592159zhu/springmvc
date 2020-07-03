b6b9b2d5a5ec13b6dd05bc242e458db8398b2af9
zhuchifeng <18515592159@163.com>
2020-07-04 03:32:11 +0800

    first commit

---
+++ springmvc_day02_03_exception/src/main/java/com/zcf/exception/SysException.java
@@ -0,0 +1,22 @@
+package com.zcf.exception;
+
+/**
+ * 自定义异常
+ */
+public class SysException extends Exception {
+    // 存储提示信息的
+    private String message;
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public SysException(String message) {
+        this.message = message;
+    }
+}
