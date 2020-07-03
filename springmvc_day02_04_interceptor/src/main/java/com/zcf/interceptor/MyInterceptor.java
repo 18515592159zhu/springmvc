ad51529a882064dacebece066c97d9c20912a524
zhuchifeng <18515592159@163.com>
2020-07-04 03:32:33 +0800

    first commit

---
+++ springmvc_day02_04_interceptor/src/main/java/com/zcf/interceptor/MyInterceptor.java
@@ -0,0 +1,30 @@
+package com.zcf.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 自定义拦截器
+ */
+public class MyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle拦截器拦截了");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle拦截器拦截了");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("afterCompletion拦截器拦截了");
+    }
+}
